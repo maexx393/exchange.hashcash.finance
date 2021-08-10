a901d9e91089b1129a411caeff65f577249ff893
maexx393 <manx393@gmail.com>
2021-08-09 21:46:21 -0700

    Update links.ts

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -2,7 +2,7 @@ import { CurrencyKey } from './currency';
 
 export const LINKS = {
 	Support: 'https://hashcash.community/docs/other',
-	Tokens: 'https://www.hashcash.money/tokens/',
+	Tokens: 'https://www.hashcash.finance/tokens/',
 	Trading: {
 		DexAG: 'https://dex.ag/',
 		Uniswap: 'https://uniswap.exchange/',
@@ -10,21 +10,21 @@ export const LINKS = {
 			`https://1inch.exchange/#/${base}/${quote}`,
 	},
 	Learn: {
-		Litepaper: 'https://www.hashcash.money/litepaper',
-		Tokens: 'https://www.hashcash.money/tokens',
-		Blog: 'https://blog.hashcash.money/',
+		Litepaper: 'https://www.hashcash.finance/litepaper',
+		Tokens: 'https://www.hashcash.finance/tokens',
+		Blog: 'https://blog.hashcash.finance/',
 	},
 	Products: {
-		Mintr: 'https://www.hashcash.money/products/mintr',
-		Dashboard: 'https://dashboard.hashcash.money/',
+		Mintr: 'https://www.hashcash.finance/products/mintr',
+		Dashboard: 'https://dashboard.hashcash.finance/',
 	},
 	Connect: {
-		Community: 'https://www.hashcash.money/community',
-		ContactUs: 'https://www.hashcash.money/contact-us',
+		Community: 'https://www.hashcash.finance/community',
+		ContactUs: 'https://www.hashcash.finance/contact-us',
 	},
 	Social: {
 		Twitter: 'https://twitter.com/hashcash_io',
-		Medium: 'https://blog.hashcash.money/',
+		Medium: 'https://blog.hashcash.finance/',
 		Discord: 'https://discordapp.com/invite/AEdUHzt',
 		GitHub: 'https://github.com/hashcashio',
 	},
@@ -34,7 +34,7 @@ export const LINKS = {
 		Messari: 'https://messari.io/asset/hashcash',
 	},
 	Blog: {
-		HowBinaryOptionsWork: 'https://blog.hashcash.money/how-binary-options-work/',
-		HowFeeReclamationRebatesWork: 'https://blog.hashcash.money/how-fee-reclamation-rebates-work/',
+		HowBinaryOptionsWork: 'https://blog.hashcash.finance/how-binary-options-work/',
+		HowFeeReclamationRebatesWork: 'https://blog.hashcash.finance/how-fee-reclamation-rebates-work/',
 	},
 };
